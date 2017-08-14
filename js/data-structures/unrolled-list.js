f64a25aa8255a8d85003094be5ea58c97361b8eb
weaverjeff <weaver_je@yahoo.com>
2017-08-13 23:27:52 -0500

    some more unrollled list changes. not done or tested

--- js/data-structures/unrolled-list.js
+++ js/data-structures/unrolled-list.js
@@ -1,24 +1,82 @@
 class UnrolledList {
    
-    constructor(capacity = 5) {
-        this.capacity = capacity;
-        this.first = new Node(this.capacity);
+    constructor(capacity = 8) {
+        this.nodeCapacity = capacity;
+        this.first = new Node(this.nodeCapacity);
+        this.last = this.first;
+        this.size = 0;
     }
-
-    get(value) {
-
+    isEmpty() {
+        return size === 0;
     }
-
+    
+    contains(value) {
+        return this.indexOf(value) !== -1;
+    }
+    indexOf(value) {
+        let node = this.first;
+        let index = 0;
+        while(node) { 
+            for (var i = 0; i < node.values.length; i++) {
+                var element =  node.values[i];
+                if (value === element) {
+                    return index + i;
+                }
+            }
+            index += this.nodeCapacity;
+            node = node.next;
+        }
+        return -1;
+    }
+    
     insert(value) {
-
+        this.insertIntoNode(this.last, this.last.size, value);
     }
+
+    insertIntoNode(node, size, value) {
+        //check if the node is full
+        if (node.size === this.nodeCapacity) {
+            let newNode = Node(this.nodeCapacity);
+            let elementsToMove = this.nodeCapacity / 2;
+            let startIndex = this.nodeCapacity - elementsToMove;
+            let i;
+            for (i = 0; i < elementsToMove; i++) {
+                newNode.values[i] = node.values[startIndex + i];
+                node.values[startIndex + i] = null;
+            }
+            node.size -= elementsToMove;
+            newNode.size = elementsToMove;
+            // insert the new node into the list
+            //todo:  is this right??
+            newNode.next = node.next;
+            node.next = newNode;
+
+            if (node == this.last) {
+                this.last = newNode;
+            }
+             // check whether the element should be inserted into
+            // the original node or into the new node
+            if (size > node.size) {
+                node = newNode;
+                size -= node.size;
+            }
+
+        }
+        
+        for (let i = node.size; i > size; i--) {
+            node.values[i] = node.values[i - 1];
+        }
+        node.values[size] = value;
+        node.size++;
+        this.size++;
+    }
+
 }
 
 class Node { 
-    constructor(max) {
-        this.values = new Array(max);
-        this.maxSize = this.values.length;
-        this.currentSize = 0;
+    constructor(nodeCapacity) {
+        this.values = new Array(nodeCapacity);
+        this.size = 0;
         this.next = null;
     }
 }
